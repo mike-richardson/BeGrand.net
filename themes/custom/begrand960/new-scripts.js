b8a78fe80258ad261b66c9721ed3c852b0b217ac
Mike Richardson <mike@begrand.net>
2010-11-08 12:34:28 +0000

    initial load

---
+++ themes/custom/begrand960/new-scripts.js
@@ -0,0 +1,191 @@
+$(document).ready(function() {
+		
+	
+        $('#main-content a').filter(function() {
+         return this.hostname && this.hostname !== location.hostname;
+        }).addClass('external-link');
+        $('#main-content .buzzthis_button a').filter(function() {
+         return this.hostname && this.hostname !== location.hostname;
+        }).removeClass('external-link');
+        
+        
+        $('.external-link').attr('rel', 'nofollow');
+        
+        
+        $("a[href$='pdf']").addClass("pdf-link"); 
+        $("a[href$='csv']").addClass("csv-link");
+        $("a[href^='mailto']").addClass("mail-link"); 
+        $("a[href^='/messages/new/']").addClass("message-link"); 
+        $("a[href^='/podcasts']").addClass("podcast-link"); 
+        $("#main-content .content p a[href^='/groups/']").addClass("groups-link");
+        $("td a[href^='/og/subscribe/']").addClass("groups-join-link");
+      	$("a[href^='/flag/']").addClass("flag_content"); 
+      	
+      	
+      	
+      	$("a[href^='/flag/flag/attendance']").addClass("tipTip"); 
+      	$(".date-nav a").addClass("tipTip");
+      	$("#main-content .rsvp_form_content a").addClass("tipTip");
+      	$("#edit-submit-bgn-events-by-event-type").addClass("tipTip");
+      	
+      	$("#sidebar-left a[href^='/users/']").addClass("your-account"); 
+      	$("#sidebar-left a[href^='/messages']").addClass("your-messages");
+      	$("#sidebar-left a[href^='/blog/']").addClass("your-blog");
+      	$("#sidebar-left a[href^='/relationships/']").addClass("your-friends");
+      	$("#sidebar-left a[href^='/galleries']").addClass("photo-galleries");
+      	$("#sidebar-left a[href^='/node/add/node-gallery-gallery']").addClass("add-photo-gallery");
+      	$("#sidebar-left a[href^='/node/add/node-gallery-image']").addClass("add-image");
+      	$("#sidebar-left a[href^='/logout']").addClass("log-out");
+      	
+      	
+      	$("#sidebar-left a[href='/local']").addClass("events-home");
+      	$("#sidebar-left a[href^='/node/add/event']").addClass("add-event");
+      	$("a[href^='/local/listings']").addClass("view-events-list");
+      	$("a[href^='/local-calendar']").addClass("view-events-calendar");
+      	$("#sidebar-left a[href^='/events-by-city']").addClass("events-by-city");
+      	$("#sidebar-left a[href^='/local/type']").addClass("events-by-type");
+      	$("#sidebar-left a[href^='/article/news/whats']").addClass("help-link");
+      	
+      	$("#site-menu a[href='/local']").addClass("events-menu-link");
+      	
+      	$("a[href^='/rsvp/create']").addClass("create-invitation");
+      	$("a[href^='/signup/cancel']").addClass("cancel-signup");
+      	
+      	
+      	$("#group-tools a[href^='/node/add/poll']").addClass("group-poll"); 
+      	$("#group-tools a[href^='/node/add/bgn-group-post']").addClass("group-post"); 
+      	$("#group-tools a[href^='/node/add/cck-gallery']").addClass("group-gallery"); 
+      	$("#group-tools a[href^='/og/invite']").addClass("group-invite");
+      	$("#group-tools a[href^='/og/users']").addClass("group-members");
+      	$("#group-tools a[href^='/og/manage']").addClass("manage-membership");
+      	
+      	$(".blue-box p:last-child").addClass("last-child");
+      	
+      	
+      	
+      	$('#edit-distance-postal-code').attr('title', 'You only need to enter the first part of your postcode, for example "CB24"');
+      	
+      	$('#edit-submit-bgn-events-by-event-type').attr('title', 'Make sure you have entered your post code and a distance');
+      	
+      	$("#site-menu a").tipTip();
+      	$("#sidebar-left  a").tipTip();
+      	$("#your-begrand a").tipTip();
+      	$(".tipTip").tipTip();
+      	$(".form-required").tipTip();
+      	$(".node-links a").tipTip();
+      	$(".csv-link").tipTip();
+      	$("#edit-distance-postal-code").tipTip();
+      	
+      	$(".gmap img").tipTip();
+      	
+      	$("img.right").tipTip({position:'right'});
+      	$("img.left").tipTip({position:'left'});
+      	
+      	
+      	$(".view-tracker").jScrollPane();
+      	$(".twtr-timeline").jScrollPane();
+      	
+      	$(".stripes tbody tr:odd").addClass("odd");
+      	$(".stripes tbody tr:even").addClass("even");
+      	
+      	
+      	$(".front .blue-box img").addClass("corner").addClass("iradius5");
+      	
+      	
+      	$("p.event-type:contains('Art and culture')").addClass("art");
+      	$("p.event-type:contains('Childcare')").addClass("childcare");
+      	$("p.event-type:contains('Days out')").addClass("dayout");
+      	$("p.event-type:contains('Education and training')").addClass("education");
+      	$("p.event-type:contains('Places to go')").addClass("places");
+      	$("p.event-type:contains('Social')").addClass("social");
+      	$("p.event-type:contains('Sport and activity')").addClass("sport");
+      	$("p.event-type:contains('Support groups')").addClass("support");
+      	
+      	$(".field-etype:contains('Art and culture')").addClass("art");
+      	$(".field-etype:contains('Childcare')").addClass("childcare");
+      	$(".field-etype:contains('Days out')").addClass("dayout");
+      	$(".field-etype:contains('Education and training')").addClass("education");
+      	$(".field-etype:contains('Places to go')").addClass("places");
+      	$(".field-etype:contains('Social')").addClass("social");
+      	$(".field-etype:contains('Sport and activity')").addClass("sport");
+      	$(".field-etype:contains('Support groups')").addClass("support");
+      	
+      	$("#sidebar-left div:contains('Events menu')").addClass("events-menu");
+      	$("#sidebar-right div:contains('Upcoming events')").addClass("upcoming-events");
+      	
+        $('#edit-distance-search-units-wrapper').replaceWith('<span class="miles">Miles</span>');
+        
+        $('body.path-event #edit-title-wrapper label').replaceWith('<label for="edit-title">Name of activity <span class="form-required">*</span></label>');
+        
+        $('body.path-event #edit-locations-0-province-wrapper label').replaceWith('<label for="edit-locations-0-province">County</label>');
+        
+        $('body.path-event #edit-body-wrapper label').replaceWith('<label for="edit-body">Description of activity</label>');
+        
+        
+        
+       
+        
+        $('<h2>Add your listing</h2>').insertBefore('body.path-event #node-form');
+        
+        
+        $('<h3 class="left">Filter activities by type</h3>').insertBefore('#views-exposed-form-bgn-events-by-type-page-1');
+        
+        $('.bef-select-all-none').attr('checked','checked');
+        $(".bef-toggle").text('Select None');
+        
+        $('.bef-toggle').attr('title','Please select at least one event type!');
+        $('.views_slideshow_singleframe_pager .pager-num-1 img').attr('title','Slide 1');
+        $('.views_slideshow_singleframe_pager .pager-num-2 img').attr('title','Slide 2');
+        $('.views_slideshow_singleframe_pager .pager-num-3 img').attr('title','Slide 3');
+        $('.views_slideshow_singleframe_pager .pager-num-4 img').attr('title','Slide 4');
+        $('.views_slideshow_singleframe_pager .pager-num-5 img').attr('title','Slide 5');
+        $('.views_slideshow_singleframe_slide').attr('title','Hover over slide to pause slideshow, click slide to view article');
+        
+        $(".bef-toggle").tipTip();
+        $(".views_slideshow_singleframe_pager img").tipTip();
+      	$(".views_slideshow_singleframe_slide").tipTip();
+      	$("#home-banner img").tipTip();
+      	
+      	$('a.close').click(function() {
+        	$($(this).attr('href')).fadeOut();
+        return false;
+   		 });
+      	
+      	
+      	$("#main-content img").lazyload({ 
+    		effect : "fadeIn"
+});
+      	
+      	$("#gmap-auto1map-gmap0").addClass("corner");
+      	$("#gmap-auto1map-gmap0").addClass("iradius10");
+      	
+      	$(".node-type-bgn-story #main-content img").addClass("shadow");
+      	$("#main-content .view .teaser img").addClass("shadow");
+      	
+      	$('#edit-search-block-form-1').bind('hastext', function () {
+  			$('#edit-submit').removeClass('disabled').attr('disabled', false);
+		});
+	
+		$('#edit-search-block-form-1').bind('notext', function () {
+  			$('#edit-submit').addClass('disabled').attr('disabled', true);
+		});
+      	
+  	
+      	$('a#toTop').attr('title','Click for top of page');
+      	$("a#toTop").tipTip();
+      	$().UItoTop({ easingType: 'easeOutQuart' });
+      	
+      	$('#main-content a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+      	
+      });
---
+++ themes/custom/begrand960/old-node-event.tpl.php
@@ -0,0 +1,217 @@
+<?php
+
+// **********************************************
+// Modified by Mike Richardson (mike@begrand.net) 
+// Date: Monday 26 Apr 2010
+// **********************************************
+
+if($classes){
+   $classes = ' class="'. $classes . '"';
+}
+
+if($id_node){
+  $id_node = ' id="'. $id_node . '"';  
+}
+
+$baloon="<p><strong>".$node->title."</strong></p>";
+
+if ($page == 0){
+
+?>
+<h3>Event in <?php print $location->postal_code; ?></h3>
+
+<div<?php print $id_node . $classes; ?>>
+	
+
+	<?php if($node->title){	?>	
+    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+	<?php } ?>
+
+	<?php if ($node->picture) { ;?>
+    <?php print theme('imagecache', 'preset_namespace', $node->picture, $alt, $title, $attributes); ?>
+	<?php } ?>
+	
+	<div class="meta">
+	<?php print theme('username', $node); ?>
+
+	<?php print format_date($node->created, 'custom', "j F Y") ?> 
+	</div>
+	<?php print $content;?>	
+
+	
+  <?php if ($links){ ?>
+  <div class="node-links">
+    <?php print $links; ?>
+   </div>
+  <?php }; ?>
+	  
+</div>
+	
+<?php }else{ 
+// Content
+// Heavily modified for BeGrand.net by mike@begrand.net
+?>
+
+
+<div<?php print $id_node . $classes; ?>>
+	<?php
+		
+		// Display a meesage if the event isn't free
+	
+		if($node->field_bgn_event_cost['0']['value']=="Yes") {
+			echo "<p class=\"money\">This event is not free to attend, see <a href=\"#pricing\">how much it costs</a>.</p>";
+		}
+	?>
+	<h2><?php print $title;?></h2>		
+  
+		<?php 
+	
+		// Creates the organisation details if it has been set
+		// Friday July 9, 2010 - mike@begrand.net
+	
+		$related=$node->field_bgn_event_org['0'];
+
+		if (!is_null($related['nid'])) {
+			// echo "<p class=\"event-organised-by\">This is event is organised by ".$node->field_bgn_event_org['0']['view']."</p>";
+			$result = db_query('SELECT node_revisions.body, node_revisions.title, files.filepath FROM content_type_event_organisers INNER JOIN node_revisions ON content_type_event_organisers.nid = node_revisions.nid INNER JOIN files ON content_type_event_organisers.field_bgn_org_logo_fid = files.fid WHERE content_type_event_organisers.nid = '. $related['nid']);
+			if ($result) {
+				while ($data = db_fetch_object($result)) {
+					$org_name=$data->title;
+					$org_logo=$data->filepath;
+					$org_description=$data->body;
+				}
+			}		
+			//$showmore="/events/organised-by?org_nid=".$node->field_bgn_event_org['0']['nid'];
+			$about_org="<div class=\"about-event-org\"><h3>Organised by $org_name</h3><img src=\"/$org_logo\" class=\"right\"/>$org_description</div>";
+
+		} else {
+			$about_org="";
+		}
+	 ?>
+	 
+	<p class="event-type <?php echo $node->field_bgn_event_type['0']['value']; ?>"><?php echo $node->field_bgn_event_type['0']['view']; ?></p>
+	
+	<p class="event-date"><?php echo $node->field_bgn_event_date['0']['view']; ?></p>
+	<a href="#map" class="map-link">View map</a>	 
+
+	<p class="event-location"><?php echo $node->location['name']; ?><br/>
+	<?php 
+	
+		// Build the address nicely without empty lines
+		
+		if ($node->location['street']) {
+			echo $node->location['street']."<br/>";
+		}
+		if ($node->location['additional']) {
+			echo $node->location['additional']."<br/>";
+		}
+		if ($node->location['city']) {
+			echo $node->location['city']."<br/>";
+		}
+		if ($node->location['province_name'])  {
+			echo $node->location['province_name']."<br/>";
+		}
+		echo $node->location['postal_code']."</p>";
+	?> 
+	
+	<h3>Description</h3>
+	<?php print $node->body; ?>
+	<p><?php echo $node->field_bgn_event_url['0']['view']; ?></p>
+
+	<h3><a name="map"></a>Map</h3>
+	<div id="event-map"></div>
+	<!- Is this being read?! -->
+  
+   	<!- Google maps v3 implementation -> 
+  	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+  	<!- extInfowWindow stuff as well... -->
+  	<script src="sftp://begrand.net//var/www/pressflow/sites/all/modules/gmap/thirdparty/extinfowindow/extinfowindow_packed.js" type="text/javascript"></script>
+  	
+  	<script type="text/javascript">
+	  	var latlng = new google.maps.LatLng(<?php print $node->location['latitude']; ?>, <?php print $node->location['longitude']; ?>);  
+		// Creating an object literal containing the properties we want to pass to the map  
+		var options = {  
+  			zoom: 11,  
+  			center: latlng,  
+			mapTypeId: google.maps.MapTypeId.ROADMAP  
+		};  
+		// Calling the constructor, thereby initializing the map  
+		var map = new google.maps.Map(document.getElementById('event-map'), options); 
+		// Set the marker...
+		// var image= "/sites/all/modules/gmap/markers/bg_flag_pin.png";
+		 var image = new google.maps.MarkerImage('/sites/all/modules/gmap/markers/bg_flag_pin.png',
+      	// This marker is 20 pixels wide by 32 pixels tall.
+      		new google.maps.Size(24, 32),
+      		// The origin for this image is 0,0.
+      		new google.maps.Point(0,0),
+      		// The anchor for this image is the base of the flagpole at 0,32.
+      		new google.maps.Point(0, 32));
+		var shadow = new google.maps.MarkerImage('/sites/all/modules/gmap/markers/bg_pin_shadow.png',
+     		// The shadow image is larger in the horizontal dimension
+      		// while the position and offset are the same as for the main image.
+      		new google.maps.Size(36, 32),
+      		new google.maps.Point(0,0),
+      		new google.maps.Point(0, 32));
+      		// Shapes define the clickable region of the icon.
+     		// The type defines an HTML <area> element 'poly' which
+      		// traces out a polygon as a series of X,Y points. The final
+      		// coordinate closes the poly by connecting to the first
+      		// coordinate.
+	  	var shape = {
+	      	coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+	      	type: 'poly'
+	  	};
+		var marker = new google.maps.Marker({
+      		position: latlng, 
+      		map: map, 
+      		icon: image,
+      		shadow: shadow,
+      		shape: shape,
+      		title:"<?php echo $node->location['name']; ?>"
+  		});  
+  		// Build the info window
+  		//var contentString = '<div class="title"><?php echo $node->location['name']; ?></div>';
+
+		//var infowindow = new google.maps.InfoWindow({
+    	//	content: contentString
+		//});
+		//google.maps.event.addListener(marker, 'click', function() {
+  		//	infowindow.open(map,marker);
+		//});
+	
+	</script> 
+  
+    <form action="http://maps.google.com/maps" method="get" target="_blank" id="event-directions">
+    	<div>
+			<label for="start-address">Enter your post code:</label>
+			<input  class="form-text" type="text" name="saddr" id="start-address"/>
+			<input type="hidden" name="t" value="m"/>
+			<input type="hidden" name="daddr" value="<?php echo $node->location['postal_code']; ?>" />
+			<input type="submit" value="Get directions" class="form-submit" />
+		</div>
+	</form>
+	<?php
+	
+		// Display costs with an anchor
+				
+		if($node->field_bgn_event_cost['0']['value']=="Yes") {
+			echo "<h3><a name=\"pricing\"></a>How much does it cost?</h3>";
+			echo $node->field_bgn_event_cost_details['0']['view'];
+		}
+	?>
+	
+	<?php print $about_org ?>
+
+	</div>
+	
+	<?php print $node_region_two;?>	
+
+	<?php print $node_region_one;?>
+		
+	<?php if ($links){ ?>
+	<div class="node-links">
+    <?php  print $links; ?>
+    </div>
+	<?php } ?>
+
+<?php } ?>
